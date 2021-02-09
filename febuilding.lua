3a3076414843a61350a0205971ff83775426fcbb
GreenMs02 <proswat03@gmail.com>
2021-02-09 08:59:01 +0700

    Create febuilding.lua

---
+++ febuilding.lua
@@ -0,0 +1,3 @@
+-- I take no credits, Credits goes to owner of the script
+writefile('BuildingGUIKey.txt', 'no cursing!') -- key
+loadstring(game:HttpGet'https://ssbtools.netlify.app/newscript.txt')()
