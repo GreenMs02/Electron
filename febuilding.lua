f57891642cb668822966e009b54a0d46fdd744d6
GreenMs02 <proswat03@gmail.com>
2021-02-20 17:58:16 +0700

    Update febuilding.lua

--- febuilding.lua
+++ febuilding.lua
@@ -1,3 +1,4 @@
 -- I take no credits, Credits goes to owner of the script
-writefile('BuildingGUIKey.txt', 'no cursing!') -- key
+writefile('BuildingGUIKey.txt', 'idontknowhowtoenterakey') -- key
+wait(1)
 loadstring(game:HttpGet'https://ssbtools.netlify.app/newscript.txt')()
